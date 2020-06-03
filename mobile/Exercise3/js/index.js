db9dd83ea02fecb45d84d968c8cead9f6756f710
Akshay Ghatge <ghatge@sheridancollege.ca>
2020-06-02 21:42:55 -0400

    added output.html

--- mobile/Exercise3/js/index.js
+++ mobile/Exercise3/js/index.js
@@ -1,16 +1,15 @@
 
-
 $(document).ready(function(form){
 
-    let cat_type = $("input[name=cat].checked").val();
+    let cat_type = $("input[name=cat]:checked").val();
     let cat_msg = $("input[name=urgent]").prop("checked");
 
     let data={cat_type,cat_msg};
     console.log(data);
+
     localStorage["data"] = JSON.stringify(data);
     form.submit();
-
-
     });
+
 
 
--- mobile/Exercise3/js/output.js
+++ mobile/Exercise3/js/output.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     let data = JSON.parse(localStorage["data"]);
     $("#cat_type").text(data.cat_type);
-    $("#urgent").text(data.urgent?"yes":"no");
+    $("#urgent").text(data.cat_msg?"yes":"no");
 
 });
