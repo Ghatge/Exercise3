185b8d20a30b2e51ed02846c46c0810cf6845ab6
Akshay Ghatge <ghatge@sheridancollege.ca>
2020-06-02 20:23:29 -0400

    added index.html ,output.html.index.js

---
+++ mobile/Exercise3/js/index.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(form){
+
+    let cat_type = $("input[name=cat].checked").val();
+    let cat_msg = $("input[name=urgent]").prop("checked");
+
+    let data={cat_type,cat_msg};
+    console.log(data);
+    localStorage["data"] = JSON.stringify(data);
+    form.submit();
+
+
+    });
+
+
