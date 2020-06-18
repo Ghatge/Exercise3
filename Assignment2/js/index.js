0024348ce0cd53c68ce3b7af052f5644455c6054
Akshay Ghatge <ghatge@sheridancollege.ca>
2020-06-18 14:17:36 -0400

    added index.html and index.js

--- Assignment2/js/index.js
+++ Assignment2/js/index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 $(function() {
     // load the xml file
     $.getJSON("cat_data/cat_data.json",displaycats);
@@ -8,18 +6,18 @@ function displaycats(data) {
     let $table = $("<table></table>");
     $table.appendTo("main");
     let cats = data.breeds;
-    breeds.forEach(function (breed, index) {
+    cats.forEach(function (breed, index) {
             $table.append(
                 `<tr> 
                 <td>
-                    <a href="index.html" data-index="${index}">
-                     <img src="images/${cats.pictures.small}" 
+                    <a href="details.html" data-index="${index}">
+                     <img src="images/${breed.pictures.small}" 
                         alt="${breed.name}" class="thumb">
                     </a> 
                 </td> 
                 <td>
-                    <span class="cat_label">${breed.label}</span>&nbsp;&nbsp;
-                    <span class="cat_price">${breed.price}</span><br>
+                    <span class="cat_label">${breed.name}</span>&nbsp;&nbsp;
+                    <span class="cat_price">${breed.origin}</span><br>
                     ${breed.text}
                 </td>
             </tr>`
