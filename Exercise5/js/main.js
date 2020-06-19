482042c5f6c65f4323f45b90039e38d5eaae0229
Akshay Ghatge <ghatge@sheridancollege.ca>
2020-06-19 16:15:55 -0400

    added main.js

---
+++ Exercise5/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#data").submit(saveInfo);
+})
+function saveInfo(form){
+    let dogName= $(`input[name=dogName]`).val();
+    let ownerName= $(`input[name=ownName]`).val();
+    let vaccinated= $(`input[name=vaccinate]`).prop("checked");
+    let dog={dogName,ownerName,vaccinated};
+
+    localStorage["dog"]=JSON.stringify(dog);
+}
